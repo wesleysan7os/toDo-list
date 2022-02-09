8039f33ee20ae566f4480db3cd9f2590a792ed92
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-02-08 22:10:29 -0300

    finishing Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Todo List</h1>
+      <button>Create Task</button>
+    </header>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Header from '../components/Header/Header';
+import TodoList from '../components/TodoList/TodoList';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <TodoList />
+    </>
+  );
+}
