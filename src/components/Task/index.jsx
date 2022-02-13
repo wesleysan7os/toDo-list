2a278d335c4c0cbe5668486f20d27c2020ffd2c7
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-02-13 19:25:27 -0300

    adding hover style && enable task editing

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -1,19 +1,43 @@
-import "./task.css";
+import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import "./task.css";
 
 export default function Task(props) {
+  const [enableTitleEdit, setEnableTitleEdit] = useState(false);
+  const [enableDescriptionEdit, setEnableDescriptionEdit] = useState(false);
+
+  function setTaskEdit(enable) {
+    setEnableTitleEdit(enable);
+    setEnableDescriptionEdit(enable);
+  }
+
   return (
     <article style={{ borderTop: `4px solid ${props.taskColor}` }}>
-      <span className="title" style={{ backgroundColor: props.titleColor }}>
+      <span
+        contentEditable={enableTitleEdit}
+        onClick={() => setEnableTitleEdit(true)}
+        className={`title ${!enableTitleEdit ? "cursor-pointer" : ""}`}
+        style={{ backgroundColor: props.titleColor }}
+      >
         {props.title}
       </span>
-      <p className="task-description">
+      <p
+        contentEditable={enableDescriptionEdit}
+        onClick={() => setEnableDescriptionEdit(true)}
+        className={`description ${
+          !enableDescriptionEdit ? "cursor-pointer" : ""
+        }`}
+      >
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim animi
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim animi
       </p>
       <footer className="icons" style={{ color: props.taskColor }}>
-        <FontAwesomeIcon icon={faPenToSquare} className="editIcon" />
+        <span
+          onClick={() => setTaskEdit(true)}
+        >
+          <FontAwesomeIcon icon={faPenToSquare} className="editIcon" />
+        </span>
         <FontAwesomeIcon icon={faTrashCan} className="trashIcon" />
       </footer>
     </article>
